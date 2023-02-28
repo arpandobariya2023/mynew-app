4175d482de043a824d7d2adca1931a360d1d30ce
arpandobariya2023 <arpandobariya2023@gmail.com>
2023-02-28 10:37:46 +0530

    add higher component in function base component

--- src/components/Footerlinks.js
+++ src/components/Footerlinks.js
@@ -1,39 +1,74 @@
 
 // // this is using function base component//////-------------------------------
-// const Footerlinks = (props) => {
-//   return (
-//     <div className="footer-1 ">
-//       <ul>
-//             {
-//               props.footernavi.map((i)=>{
-//                 return(
-//                   <li>{i}</li>
-//                 )
-//               })
-//             }
-//         </ul>
+const Footerlinks = (props) => {
+  
+  
+  const Text =()=>{
+    return(
+      <div>
+        <h1>hello world</h1>
+      </div>
+    )
+  }
+  const HOC = (Wrapper , color) =>{
+      return(props)=>(
+        <div style={{color : color}}>
+          <Wrapper {...props}/>
+        </div>
+      )
+  }
+  const Comp = HOC(Text,'red')
+
+  return (
+    <div className="footer-1 ">
+      <ul>
+            {
+              props.footernavi.map((i)=>{
+                return(
+                  <li>{i}</li>
+                )
+              })
+            }
+        </ul>
+        <Comp />
+    </div>
+  )
+}
+
+export default Footerlinks
+
+
+// // this is using class base component//////-------------------------------
+// import React, { Component } from 'react'
+
+// export default class Footerlinks extends Component {
+// Text = () => {
+//   return
+//     <div>
+//       <h1>hello world</h1>
+//     </div>
+// }
+// HOC = (wrapper , color) =>{
+//   return(this.props)(
+//     <div>
+//       <wrapper {...this.props}/>
 //     </div>
 //   )
 // }
+// Comp = HOC(this.Text,this.color)
 
-// export default Footerlinks
 
-
-// this is using class base component//////-------------------------------
-import React, { Component } from 'react'
-
-export default class Footerlinks extends Component {
-
-  render() {
-    return (
-      <div className="footer-1 ">
-      <ul>
-             <li>Instragram</li>
-             <li>Facebook</li>
-             <li>Twitter</li>
-         </ul>
-     </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div className="footer-1 ">
+//         <ul>
+//              <li>Instragram</li>
+//              <li>Facebook</li>
+//              <li>Twitter</li>
+//          </ul>
+//          <Comp />
+//      </div>
+//     )
+//   }
+// }
 
