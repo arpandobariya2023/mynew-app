0db100b85d47f495aa78006cf1ce398487d3aa72
arpandobariya2023 <arpandobariya2023@gmail.com>
2023-02-28 10:20:20 +0530

    new

--- src/components/Footerlinks.js
+++ src/components/Footerlinks.js
@@ -1,39 +1,39 @@
 
-// this is using function base component//////-------------------------------
-const Footerlinks = (props) => {
-  return (
-    <div className="footer-1 ">
-      <ul>
-            {
-              props.footernavi.map((i)=>{
-                return(
-                  <li>{i}</li>
-                )
-              })
-            }
-        </ul>
-    </div>
-  )
-}
+// // this is using function base component//////-------------------------------
+// const Footerlinks = (props) => {
+//   return (
+//     <div className="footer-1 ">
+//       <ul>
+//             {
+//               props.footernavi.map((i)=>{
+//                 return(
+//                   <li>{i}</li>
+//                 )
+//               })
+//             }
+//         </ul>
+//     </div>
+//   )
+// }
 
-export default Footerlinks
+// export default Footerlinks
 
 
 // this is using class base component//////-------------------------------
-// import React, { Component } from 'react'
+import React, { Component } from 'react'
 
-// export default class Footerlinks extends Component {
+export default class Footerlinks extends Component {
 
-//   render() {
-//     return (
-//       <div className="footer-1 ">
-//       <ul>
-//              <li>Instragram</li>
-//              <li>Facebook</li>
-//              <li>Twitter</li>
-//          </ul>
-//      </div>
-//     )
-//   }
-// }
+  render() {
+    return (
+      <div className="footer-1 ">
+      <ul>
+             <li>Instragram</li>
+             <li>Facebook</li>
+             <li>Twitter</li>
+         </ul>
+     </div>
+    )
+  }
+}
 
